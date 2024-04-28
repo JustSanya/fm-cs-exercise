5c93f0a6ddfd95dc416b23cf60bf9767ccbf2047
Alexander <alexander.shyrykalov@gmail.com>
2024-04-28 13:52:32 +0300

    init

---
+++ bubbleSort.spec.js
@@ -0,0 +1,26 @@
+function bubbleSort(nums) {
+  const arr = new Array(...nums);
+
+  let unsortedLength = arr.length;
+
+  for (let j = 0; j < unsortedLength; j++) {
+    for (let i = 0; i < arr.length - j; i++) {
+      if (arr[i] > arr[i + 1]) {
+        const temp = arr[i + 1];
+        arr[i + 1] = arr[i];
+        arr[i] = temp;
+      }
+    }
+
+  }
+  console.log(arr);
+  return arr;
+}
+
+// unit tests
+// do not modify the below code
+test("bubble sort", function () {
+  const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+  const sortedNums = bubbleSort(nums);
+  expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+});
